18a1d7a310bfbf0b202c1ef461633914b169738f
Dharanaesh2001 <108856772+Dharanaesh2001@users.noreply.github.com>
2022-07-07 13:54:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("generate");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+=";
+
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword() {
+    const len = lenEl.value;
+    if(len>30){
+        alert("MAXIMUM SIZE IS 30");
+    }
+    else if(len<2){
+        alert("MINIMUM SIZE IS 2");
+    }
+    else{
+    let password = "";
+    for (let i = password.length; i < len; i++) {
+        const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+    if (xs.length === 0){
+        alert("NOTHING IS SELECTED");
+        break;
+    }
+    else{
+     const x= xs[Math.floor(Math.random() * xs.length)];  
+     password += x;
+     pwEl.innerText = password;
+    }
+  }
+}
+}
+
+function generateX() {
+    
+    if (xs.length === 0)
+        return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
+
+copyEl.addEventListener("click", () => {
+    const textarea = document.createElement("textarea");
+    const password = pwEl.innerText;
+    if (!password) {
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    textarea.remove();
+    alert("Password copied to clipboard");
+});
